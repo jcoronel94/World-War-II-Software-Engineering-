5fc367d761c972a007c2923d0b5afced3a00f2d5
jcoronel94 <coronejo@kean.edu>
2015-10-17 20:34:09 -0400

    Create tester3.php

---
+++ tester3.php
@@ -0,0 +1,211 @@
+<!DOCTYPE HTML>
+<!--
+	Helios by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+		<html>
+			<head>
+				<title>Nancy Thompson WWII Scrapbook</title>
+
+	 
+				<meta charset="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+				<link rel="stylesheet" href="assets/css/main.css" />
+				<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+				 <script type="text/javascript" src="assets/js/jquery-2.1.1.js"></script>
+
+
+				  <!-- small script for dropdown redirection -->
+		   <script type="text/javascript">
+
+		   //listen when the document is ready
+		   $(document).ready(function() {
+
+		      $("#yearDropdown").on('change', function() {
+
+		          if($("#yearDropdown option:selected").index() !== 0) {
+
+	            window.location.href = $("#yearDropdown").val();
+	          }
+	      }); //end select listener
+
+	    }); //end doc ready listener
+
+	   </script>
+
+
+
+
+
+	</head>
+	<body class="no-sidebar">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+
+					<!-- Inner -->
+						<div class="inner">
+							<header>
+								<h1><a href="no-sidebar.html" id="logo">Search</a></h1>
+							</header>
+						</div>
+
+						<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li><a href="index.html">Home</a></li>
+								<li>
+									<a href="#">Reading The Scrapbook</a>
+									<ul>
+										<li><a href="tester1.php">Table of Contents</a></li>
+										<li><a href="tester2.php">Search</a></li>
+										<li><a href="tester3.php">Map by Year</a></li>
+										<li>
+											<a href="#">And a submenu &hellip;</a>
+											<ul>
+												<li><a href="#">Lorem ipsum dolor</a></li>
+												<li><a href="#">Phasellus consequat</a></li>
+												<li><a href="#">Magna phasellus</a></li>
+												<li><a href="#">Etiam dolore nisl</a></li>
+											</ul>
+										</li>
+										
+									</ul>
+								</li>
+								<li><a href="index#slide">Experiencing WWII</a>
+									<ul>
+										<li><a href="index.html#letter writing">Letter Writing</a></li>
+										<li><a href="index.html#wartime">Wartime Experience</a></li>
+										<li><a href="index.html#homefront">Homefront</a></li>
+										<li><a href="index.html#overseas">Overseas Experience</a></li>
+										<li><a href="index.html#discovering">Discovering America</a></li>
+										<li><a href="index.html#race">Race</a></li>
+										<li><a href="index.html#women">Women</a></li>
+										<li><a href="index.html#teachers">Teachers and Training</a></li>
+										<li><a href="index.html#postwar">Postwar Education</a></li>
+										<li><a href="index.html#thefallen">The Fallen</a></li>
+										<li><a href="index.html#memory">Memory</a></li>
+									</ul>
+								</li>
+								
+								<li><a href="index.html#scrapbooking the war">Scrapbooking the War</a>
+								  <ul>
+											 <li><a href="index.html#Nancy">Nancy Thomspon</a></li>
+											 <li><a href="index.html#newark">Newark State Teacher's College</a>
+											 	 <ul>
+												 <li><a href="#">School Newspaper</a></li>
+												 <li><a href="#">Yearbooks</a></li>
+												 <li><a href="#">Course Catalog</a></li>
+												</ul>
+											</li>
+											 <li><a href="index.html#scrapbook">Scrapbook</a></li>
+											 <li><a href="index.html#serviceman">Serviceman's News</a></li>
+											 <li><a href="index.html#project">Project History</a></li>
+								  </ul>		
+								</li>
+								
+								
+								<li><a href="#">Lesson Plans</a></li>
+									<li><a href="#">Historical Analysis</a></li>
+							</ul>
+						</nav>
+
+				</div>
+
+
+			<!-- Main -->
+		
+		 <div class="wrapper style1">
+				
+			<body>
+    	
+     
+
+				
+				
+				    <select name="yearDropdown" id="yearDropdown"  style="color:black">
+    		    <option value="">----</option>
+    				<option value="viewyear.php?year=All">All</option>
+    				<option value="viewyear.php?year=1941">1941</option>
+    				<option value="viewyear.php?year=1942">1942</option>
+    				<option value="viewyear.php?year=1943">1943</option>
+    				<option value="viewyear.php?year=1944">1944</option>
+    				<option value="viewyear.php?year=1945">1945</option>
+    				<option value="viewyear.php?year=1946">1946</option>
+    				<option value="viewyear.php?year=1950">1950</option>
+  					</select>
+						
+					
+
+      
+    
+   </body>
+
+	</div>
+				
+
+			<!-- Footer -->
+				<div id="footer">
+					<div class="container">
+						
+
+							
+
+						
+								
+
+						
+
+						
+						
+						<div class="row">
+							<div class="12u">
+
+								<!-- Contact -->
+									<section class="contact">
+										<header>
+											<h3>Nisl turpis nascetur interdum?</h3>
+										</header>
+										<p>Urna nisl non quis interdum mus ornare ridiculus egestas ridiculus lobortis vivamus tempor aliquet.</p>
+										<ul class="icons">
+											<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+											<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+											<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+											<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+											<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+											<li><a href="#" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
+										</ul>
+									</section>
+
+								<!-- Copyright -->
+									<div class="copyright">
+										<ul class="menu">
+											<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+										</ul>
+									</div>
+
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.onvisible.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+		
+ 			<script src="assets/bootstrap/js/bootstrap.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
